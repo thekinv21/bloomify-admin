40597aad29f3c63397090e68da65ad1c80196e3f
Vadim Kiniabaev <thekinv21@gmail.com>
2024-11-30 00:47:50 +0300

    feat:[BMF-12][Vadim]: key constant ve path constant eklendi

---
+++ src/constant/key.constant.ts
@@ -0,0 +1,5 @@
+export const keyConstant = {
+	login: 'login',
+	register: 'register',
+	get_me: 'get-me'
+} as const
---
+++ src/constant/path.constant.ts
@@ -0,0 +1,7 @@
+export const pathConstant = {
+	initial: '/',
+	login: '/auth/login',
+	register: '/auth/register',
+	home: '/home',
+	flowers: '/flowers'
+} as const
