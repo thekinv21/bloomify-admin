b172b767b19b439d495328941017d49d7dfc5c76
Vadim Kiniabaev <thekinv21@gmail.com>
2024-11-29 22:36:29 +0300

    feat:[BMF-12][Vadim]: auth servisleri eklendi

---
+++ src/services/auth.service.ts
@@ -0,0 +1,45 @@
+import { API_URL, axiosClassic, axiosWithAuth } from './axios/axios'
+import {
+	IGetMeResponse,
+	ILoginRequest,
+	ILoginResponse,
+	ILogoutRequest,
+	IRegisterRequest
+} from '@/types'
+
+class AuthService {
+	private BASE_URL = `${API_URL}/api/auth`
+
+	async login(request: ILoginRequest) {
+		const response = await axiosClassic.post<ILoginResponse>(
+			`${this.BASE_URL}/login`,
+			request
+		)
+		return response
+	}
+
+	async register(request: IRegisterRequest) {
+		const response = await axiosClassic.post<IRegisterRequest>(
+			`${this.BASE_URL}/login`,
+			request
+		)
+		return response
+	}
+
+	async getMe() {
+		const response = await axiosWithAuth.get<IGetMeResponse>(
+			`${this.BASE_URL}/get-myself`
+		)
+		return response
+	}
+
+	async logout(request: ILogoutRequest) {
+		const response = await axiosWithAuth.post<IGetMeResponse>(
+			`${this.BASE_URL}/logout`,
+			request
+		)
+		return response
+	}
+}
+
+export const authService = new AuthService()
