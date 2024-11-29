d2eedb6f4dff73974ea15a0e7d74478c4337d276
Vadim Kiniabaev <thekinv21@gmail.com>
2024-11-30 00:51:59 +0300

    feat:[BMF-12][Vadim]: hook ve constantların kısa yolları eklendi

---
+++ src/constant/index.ts
@@ -0,0 +1,4 @@
+import { keyConstant } from './key.constant'
+import { pathConstant } from './path.constant'
+
+export { keyConstant, pathConstant }
---
+++ src/hooks/index.ts
@@ -0,0 +1,3 @@
+import { useRoute } from './useRoute'
+
+export { useRoute }
