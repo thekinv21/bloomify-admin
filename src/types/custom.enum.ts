023a301d15e7570a88d7e4924ee3d046e1b2590a
Vadim Kiniabaev <thekinv21@gmail.com>
2024-11-29 22:00:59 +0300

    feat:[BMF-12][Vadim]: custom enum eklendi , http token ve role enumlar

---
+++ src/types/custom.enum.ts
@@ -0,0 +1,19 @@
+export enum RoleEnum {
+	USER = 'USER',
+	ADMIN = 'ADMIN',
+	SUPER_ADMIN = 'SUPERADMIN'
+}
+
+export enum TokenEnum {
+	ACCESS_TOKEN = 'accessToken',
+	REFRESH_TOKEN = 'refreshToken',
+	USER = 'user'
+}
+
+export enum HttpEnum {
+	POST = 'post',
+	PUT = 'put',
+	PATCH = 'patch',
+	GET = 'get',
+	DELETE = 'delete'
+}
