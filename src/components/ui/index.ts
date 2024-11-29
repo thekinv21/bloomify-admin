da2de79df837c9bee8ce44a5f591ebc2ca86eb74
Vadim Kiniabaev <thekinv21@gmail.com>
2024-11-29 21:13:36 +0300

    feat:[BMF-11][Vadim]: kart input ve label eklendi

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,23 @@
 import { Button } from './button'
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle
+} from './card'
+import { Input } from './input'
+import { Label } from './label'
 
-export { Button }
+export {
+	Button,
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+	Input,
+	Label
+}
