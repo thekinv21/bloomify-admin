29318176f164d78a9d54dc98ad9db819c7a46e5f
Vadim Kiniabaev <thekinv21@gmail.com>
2024-11-29 19:29:14 +0300

    feat:[BMF-11][Vadim]: login, ana sayfa sayfalarının taslak hali  eklendi

---
+++ src/screens/home/index.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+	return (
+		<div className='flex h-screen w-full items-center justify-center'>
+			Ana Sayfa
+		</div>
+	)
+}
---
+++ src/screens/index.ts
@@ -0,0 +1,4 @@
+import LoginPage from './auth/login'
+import HomePage from '@/screens/home'
+
+export { HomePage, LoginPage }
