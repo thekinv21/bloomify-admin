0e929a9f8b704ea0f19bb4abbf6550ab6013fc87
Vadim Kiniabaev <thekinv21@gmail.com>
2024-11-29 22:36:46 +0300

    feat:[BMF-12][Vadim]: auth tipleri eklendi

---
+++ src/types/auth.types.ts
@@ -0,0 +1,33 @@
+import { IUser } from './user.types'
+
+export interface ILoginResponse {
+	accessToken: string
+	user: IUser
+}
+
+export interface IGetMeResponse {
+	user: IUser
+}
+
+export interface IRegisterResponse {
+	user: IUser
+}
+
+export interface ILoginRequest {
+	username: string
+	password: string
+}
+
+export interface IRegisterRequest {
+	username: string
+	email: string
+	firstName?: string
+	lastName?: string
+	password: string
+	avatarPath?: string
+	isActive?: boolean
+}
+
+export interface ILogoutRequest {
+	token: string
+}
