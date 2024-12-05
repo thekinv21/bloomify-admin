82b66832db70ef9ea64cfc5b10ba2403fa9ec23a
Vadim Kiniabaev <thekinv21@gmail.com>
2024-12-05 23:53:59 +0300

    feat: add CustomTooltip component and Tooltip utility for enhanced UI tooltips

---
+++ src/components/ui/custom/CustomTooltip.tsx
@@ -0,0 +1,41 @@
+import { ReactNode } from 'react'
+
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger
+} from '@/components/ui/tooltip'
+
+interface ICustomTooltip {
+	children: ReactNode
+	message: string
+	position?: 'top' | 'right' | 'bottom' | 'left'
+	offset?: number
+	align?: 'start' | 'center' | 'end'
+	isLink?: boolean
+	url?: string
+}
+
+export function CustomTooltip({ children, ...props }: ICustomTooltip) {
+	return (
+		<TooltipProvider>
+			<Tooltip>
+				<TooltipTrigger asChild>{children}</TooltipTrigger>
+				<TooltipContent
+					side={props.position}
+					sideOffset={props.offset}
+					align={props.align}
+				>
+					{props.isLink ? (
+						<a href={props.url?.toString()} className='cursor-pointer'>
+							{props.message}
+						</a>
+					) : (
+						<span className='cursor-text'>{props.message}</span>
+					)}
+				</TooltipContent>
+			</Tooltip>
+		</TooltipProvider>
+	)
+}
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -8,6 +8,7 @@ import {
 	CardTitle
 } from './card'
 import CustomInput from './custom/CustomInput'
+import { CustomTooltip } from './custom/CustomTooltip'
 import { Input } from './input'
 import { Label } from './label'
 import { Loader } from './loader/Loader'
@@ -21,6 +22,7 @@ export {
 	CardHeader,
 	CardTitle,
 	CustomInput,
+	CustomTooltip,
 	Input,
 	Label,
 	Loader
