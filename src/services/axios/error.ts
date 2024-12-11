2041c1b74bfff68400a587fa4513f24c0f975f8f
Vadim Kiniabaev <thekinv21@gmail.com>
2024-12-11 23:34:19 +0300

    fix: update error handling to use 'message' instead of 'error' in Axios response

--- src/services/axios/error.ts
+++ src/services/axios/error.ts
@@ -3,6 +3,6 @@ import { AxiosError } from 'axios'
 export const errorCatch = (error: AxiosError): string =>
 	error.response && error.response.data
 		? typeof error.response.data === 'object'
-			? (error.response.data as { error: string }).error
+			? (error.response.data as { message: string }).message
 			: (error.response.data as string)
 		: error.message
