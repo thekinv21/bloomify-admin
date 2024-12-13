2059558675a32ad0c6eef201dec84995040f7172
Vadim Kiniabaev <thekinv21@gmail.com>
2024-12-13 13:32:47 +0300

    chore: update framer-motion and motion packages to version 11.14.3 and comment out build configuration in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -16,24 +16,24 @@ export default defineConfig({
 				silenceDeprecations: ['legacy-js-api']
 			}
 		}
-	},
-
-	build: {
-		chunkSizeWarningLimit: 600,
-		rollupOptions: {
-			output: {
-				manualChunks(id: string) {
-					if (id.indexOf('node_modules') !== -1) {
-						const basic = id.toString().split('node_modules/')[1]
-						const sub1 = basic.split('/')[0]
-						if (sub1 !== '.pnpm') {
-							return sub1.toString()
-						}
-						const name2 = basic.split('/')[1]
-						return name2.split('@')[name2[0] === '@' ? 1 : 0].toString()
-					}
-				}
-			}
-		}
 	}
+
+	// build: {
+	// 	chunkSizeWarningLimit: 600,
+	// 	rollupOptions: {
+	// 		output: {
+	// 			manualChunks(id: string) {
+	// 				if (id.indexOf('node_modules') !== -1) {
+	// 					const basic = id.toString().split('node_modules/')[1]
+	// 					const sub1 = basic.split('/')[0]
+	// 					if (sub1 !== '.pnpm') {
+	// 						return sub1.toString()
+	// 					}
+	// 					const name2 = basic.split('/')[1]
+	// 					return name2.split('@')[name2[0] === '@' ? 1 : 0].toString()
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// }
 })
