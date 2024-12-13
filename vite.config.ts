2c9e06e91f9c367f6d8cbc8ab38747ab54536e4d
Vadim Kiniabaev <thekinv21@gmail.com>
2024-12-13 14:15:27 +0300

    fix: remove chunk size warning limit and manual chunking logic from Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -16,28 +16,5 @@ export default defineConfig({
 				silenceDeprecations: ['legacy-js-api']
 			}
 		}
-	},
-
-	build: {
-		chunkSizeWarningLimit: 600,
-		rollupOptions: {
-			output: {
-				manualChunks(id) {
-					if (id.includes('node_modules')) {
-						const parts = id.split('node_modules/')[1]
-						const packageName = parts.split('/')[0]
-
-						if (packageName !== '.pnpm') {
-							return packageName
-						}
-
-						const nestedParts = parts.split('/')
-						const name =
-							nestedParts[1]?.split('@')[nestedParts[1]?.[0] === '@' ? 1 : 0]
-						return name || 'vendor'
-					}
-				}
-			}
-		}
 	}
 })
