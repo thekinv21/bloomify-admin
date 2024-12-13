41a5d0f27db7240d5f794f4aa4dda0c9dc55e516
Vadim Kiniabaev <thekinv21@gmail.com>
2024-12-13 15:33:34 +0300

    fix: add nginx configuration for multiple applications and update Vite build chunk size warning limit

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,24 @@ export default defineConfig({
 
 	server: {
 		port: 5173
+	},
+
+	build: {
+		chunkSizeWarningLimit: 600,
+		rollupOptions: {
+			output: {
+				manualChunks(id: string) {
+					if (id.indexOf('node_modules') !== -1) {
+						const basic = id.toString().split('node_modules/')[1]
+						const sub1 = basic.split('/')[0]
+						if (sub1 !== '.pnpm') {
+							return sub1.toString()
+						}
+						const name2 = basic.split('/')[1]
+						return name2.split('@')[name2[0] === '@' ? 1 : 0].toString()
+					}
+				}
+			}
+		}
 	}
 })
