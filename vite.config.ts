a60d51161188af4fb62b7d4c77bda5f5fd3e7eec
Vadim Kiniabaev <thekinv21@gmail.com>
2024-12-13 10:44:41 +0300

    feat: enhance Vite configuration with chunk size warning limit and manual chunking for better build optimization

--- vite.config.ts
+++ vite.config.ts
@@ -6,15 +6,34 @@ export default defineConfig({
 	plugins: [react()],
 	resolve: {
 		alias: {
-			'@': path.resolve(__dirname, './src'),
-		},
+			'@': path.resolve(__dirname, './src')
+		}
 	},
 
 	css: {
 		preprocessorOptions: {
 			scss: {
-				silenceDeprecations: ['legacy-js-api'],
-			},
-		},
+				silenceDeprecations: ['legacy-js-api']
+			}
+		}
 	},
+
+	build: {
+		chunkSizeWarningLimit: 600,
+		rollupOptions: {
+			output: {
+				manualChunks(id: string) {
+					if (id.indexOf('node_modules') !== -1) {
+						const basic = id.toString().split('node_modules/')[1]
+						const sub1 = basic.split('/')[0]
+						if (sub1 !== '.pnpm') {
+							return sub1.toString()
+						}
+						const name2 = basic.split('/')[1]
+						return name2.split('@')[name2[0] === '@' ? 1 : 0].toString()
+					}
+				}
+			}
+		}
+	}
 })
