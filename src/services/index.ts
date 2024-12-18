9341c7ceb2919bd19a497c50ccfb234fe7c7cdf6
Vadim Kiniabaev <thekinv21@gmail.com>
2024-12-18 22:05:51 +0300

    fix: add roleService and userService imports to index for improved service accessibility

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,6 @@
+import { roleService } from './admin/role.service'
+import { userService } from './admin/user.service'
 import { authService } from './auth.service'
 import { errorCatch } from './axios/error'
 
-export { authService, errorCatch }
+export { authService, errorCatch, roleService, userService }
