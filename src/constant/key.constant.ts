1404db558b0674d3ad9dafaf2fa759606d16aed2
Vadim Kiniabaev <thekinv21@gmail.com>
2024-12-18 22:06:03 +0300

    fix: add user-related key constants for improved API interaction

--- src/constant/key.constant.ts
+++ src/constant/key.constant.ts
@@ -2,5 +2,11 @@ export const keyConstant = {
 	login: 'login',
 	register: 'register',
 	get_me: 'get-me',
-	logout: 'logout'
+	logout: 'logout',
+	get_users: 'get-all-users',
+	get_user: 'get-user-by-id',
+	create_user: 'create-user',
+	edit_user: 'edit-user',
+	delete_user: 'delete-user',
+	toggle_user: 'toggle-user'
 } as const
