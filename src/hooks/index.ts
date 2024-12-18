c7573b6342a09b4319153b287c66b1a559aa9b24
Vadim Kiniabaev <thekinv21@gmail.com>
2024-12-18 23:55:21 +0300

    feat: add useDebounce hook and export it from hooks index

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,7 @@
 import { useCookie } from './useCookie'
+import { useDebounce } from './useDebounce'
 import { useResponsive } from './useResponsive'
 import { useRoute } from './useRoute'
 import { useTranslate } from './useTranslate'
 
-export { useCookie, useResponsive, useRoute, useTranslate }
+export { useCookie, useDebounce, useResponsive, useRoute, useTranslate }
