f15f5e6859b6beb7025a7cd6d4e8e70d3e4c0084
Vadim Kiniabaev <thekinv21@gmail.com>
2024-12-19 20:04:05 +0300

    feat: enhance DataTable component with add button and responsive layout adjustments

--- src/components/ui/dataTable/DataTableHeading.tsx
+++ src/components/ui/dataTable/DataTableHeading.tsx
@@ -1,7 +1,8 @@
-import { Search } from 'lucide-react'
+import { Plus, Search } from 'lucide-react'
 
 import { useTranslate } from '@/hooks'
 
+import { Button } from '../button'
 import CustomInput from '../custom/CustomInput'
 
 import styles from './DataTable.module.scss'
@@ -10,6 +11,8 @@ interface IDataTableHeading {
 	searchTerm: string
 	setSearchTerm: (v: string) => void
 	heading: string
+	navigateTo?: string
+	setIsOpen?: (v: boolean) => void
 }
 
 export function DataTableHeading(props: IDataTableHeading) {
@@ -18,14 +21,27 @@ export function DataTableHeading(props: IDataTableHeading) {
 	return (
 		<div className={styles.custom_table_heading}>
 			<h1 className={styles.heading}>{props.heading}</h1>
-			<div className={styles.input_container}>
-				<CustomInput
-					type='text'
-					placeholder={t('search')}
-					value={props.searchTerm}
-					onChange={e => props.setSearchTerm(e.target.value)}
-					iconLeft={<Search size={20} />}
-				/>
+			<div className={styles.block_container}>
+				<div className={styles.input_container}>
+					<CustomInput
+						type='text'
+						placeholder={t('search')}
+						value={props.searchTerm}
+						onChange={e => props.setSearchTerm(e.target.value)}
+						iconLeft={<Search size={20} />}
+					/>
+				</div>
+
+				{props.setIsOpen && (
+					<Button
+						size='icon'
+						leftSection={<Plus size={16} />}
+						className='w-full text-xs font-semibold uppercase sm:w-24 sm:pr-3'
+						onClick={() => props.setIsOpen?.(true)}
+					>
+						{t('add')}
+					</Button>
+				)}
 			</div>
 		</div>
 	)
