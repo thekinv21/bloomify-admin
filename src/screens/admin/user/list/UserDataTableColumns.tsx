896d9b7eb75532bab9a11871cbf9a10d13482d63
Vadim Kiniabaev <thekinv21@gmail.com>
2024-12-19 13:12:34 +0300

    feat: integrate toggle functionality in UserDataTable and enhance Alert component

--- src/screens/admin/user/list/UserDataTableColumns.tsx
+++ src/screens/admin/user/list/UserDataTableColumns.tsx
@@ -1,43 +1,71 @@
 import { ColumnDef } from '@tanstack/react-table'
-import { FilePenLineIcon, PlayCircleIcon, Trash2Icon } from 'lucide-react'
+import { FilePenLineIcon, Trash2Icon } from 'lucide-react'
 import { useMemo } from 'react'
 
 import { useTranslate } from '@/hooks'
 
 import { IUser } from '@/types'
 
+import { Alert, Switch } from '@/components/ui'
+
+import { useToggleUser } from '../hooks/useToggleUser'
+
+import { DateShowFormat } from '@/utils'
+
 export const UserDataTableColumns = () => {
 	const { t } = useTranslate()
+
+	const { toggleUser } = useToggleUser()
 
 	const columns = useMemo<ColumnDef<IUser>[]>(
 		() => [
 			{
 				accessorKey: 'firstName',
-				header: () => <span>{t('firstName')}</span>,
+				header: () => <>{t('firstName')}</>,
 				cell: info => info.getValue()
 			},
 			{
 				accessorKey: 'lastName',
-				header: () => <span>{t('lastName')}</span>
+				header: () => <>{t('lastName')}</>
 			},
 			{
 				accessorKey: 'username',
-				header: () => <span>{t('username')}</span>
-			},
-			{
-				accessorKey: 'isActive',
-				header: () => <span>{t('status')}</span>,
-				cell: info => (info.getValue() ? t('active') : t('passive'))
+				header: () => <>{t('username')}</>
 			},
 
 			{
 				accessorKey: 'createdAt',
-				header: () => <span>{t('createdAt')}</span>
+				header: () => <>{t('createdAt')}</>,
+				cell: info => <>{DateShowFormat(info.getValue() as string)}</>
 			},
 
 			{
 				accessorKey: 'updatedAt',
-				header: () => <span>{t('updatedAt')}</span>
+				header: () => <>{t('updatedAt')}</>,
+				cell: info => <>{DateShowFormat(info.getValue() as string)}</>
+			},
+
+			{
+				accessorKey: 'isActive',
+				header: () => <>{t('status')}</>,
+				cell: info => {
+					const row = info.row.original
+					const ID = row.id
+
+					return (
+						<Switch
+							checked={info.getValue() as boolean}
+							onClick={() => {
+								Alert({
+									subTitle: t('toggle'),
+									action: async () => {
+										return toggleUser(ID)
+									}
+								})
+							}}
+						/>
+					)
+				}
 			},
 
 			{
@@ -47,12 +75,6 @@ export const UserDataTableColumns = () => {
 					const id = info.getValue() as string
 					return (
 						<div className='flex items-start gap-2'>
-							<button
-								title={t(`toggle`)}
-								onClick={() => alert(`Toggle Id = ${id}`)}
-							>
-								<PlayCircleIcon color='green' size={20} />
-							</button>
 							<button
 								title={t(`edit`)}
 								onClick={() => alert(`Edit Id = ${id}`)}
