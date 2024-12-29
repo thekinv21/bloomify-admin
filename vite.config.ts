841ec1db1b37fdfdffdbdc1187aba11d131bc23c
Vadim Kiniabaev <thekinv21@gmail.com>
2024-12-29 04:01:42 +0300

    bug:[master-xxx][Vadim]: vite config chunks cannot be fixed

--- vite.config.ts
+++ vite.config.ts
@@ -8,45 +8,5 @@ export default defineConfig({
 		alias: {
 			'@': path.resolve(__dirname, './src')
 		}
-	},
-
-	build: {
-		chunkSizeWarningLimit: 600,
-		rollupOptions: {
-			output: {
-				manualChunks(id) {
-					if (id.includes('node_modules')) {
-						const modulePath = id.split('node_modules/')[1]
-						const parts = modulePath.split('/')
-
-						if (parts[0] !== '.pnpm') {
-							return parts[0]
-						}
-
-						const scopedPackageName = parts[1]
-						const chunkName =
-							scopedPackageName.split('@')[
-								scopedPackageName.startsWith('@') ? 1 : 0
-							]
-
-						if (
-							chunkName &&
-							chunkName !== 'detect-node-es' &&
-							chunkName !== 'dom-helpers' &&
-							chunkName !== 'html-parse-stringify' &&
-							chunkName !== 'set-cookie-parser' &&
-							chunkName !== 'turbo-stream' &&
-							chunkName !== 'void-elements'
-						) {
-							return chunkName
-						}
-
-						return null
-					}
-
-					return null
-				}
-			}
-		}
 	}
 })
