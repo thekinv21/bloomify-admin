515a8a1b85f3347c8982bfa253453b767158cfd5
Vadim Kiniabaev <thekinv21@gmail.com>
2024-12-29 03:50:33 +0300

    fix:[master-xxx][Vadim]: fixed vite config for build warnings

--- vite.config.ts
+++ vite.config.ts
@@ -10,24 +10,41 @@ export default defineConfig({
 		}
 	},
 
-	server: {
-		port: 5173
-	},
-
 	build: {
 		chunkSizeWarningLimit: 600,
 		rollupOptions: {
 			output: {
-				manualChunks(id: string) {
-					if (id.indexOf('node_modules') !== -1) {
-						const basic = id.toString().split('node_modules/')[1]
-						const sub1 = basic.split('/')[0]
-						if (sub1 !== '.pnpm') {
-							return sub1.toString()
+				manualChunks(id) {
+					if (id.includes('node_modules')) {
+						const modulePath = id.split('node_modules/')[1]
+						const parts = modulePath.split('/')
+
+						if (parts[0] !== '.pnpm') {
+							return parts[0]
 						}
-						const name2 = basic.split('/')[1]
-						return name2.split('@')[name2[0] === '@' ? 1 : 0].toString()
+
+						const scopedPackageName = parts[1]
+						const chunkName =
+							scopedPackageName.split('@')[
+								scopedPackageName.startsWith('@') ? 1 : 0
+							]
+
+						if (
+							chunkName &&
+							chunkName !== 'detect-node-es' &&
+							chunkName !== 'dom-helpers' &&
+							chunkName !== 'html-parse-stringify' &&
+							chunkName !== 'set-cookie-parser' &&
+							chunkName !== 'turbo-stream' &&
+							chunkName !== 'void-elements'
+						) {
+							return chunkName
+						}
+
+						return null
 					}
+
+					return null
 				}
 			}
 		}
