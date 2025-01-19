0a88e5a02f59c2654d1fde1c0da048ae468f99e5
Vadim Kiniabaev <thekinv21@gmail.com>
2025-01-19 23:18:34 +0300

    refactor: update SidebarLogo component and adjust color variables in index.scss

--- src/components/layout/sidebar/SidebarLogo.tsx
+++ src/components/layout/sidebar/SidebarLogo.tsx
@@ -10,15 +10,24 @@ export function SidebarLogo({ sidebar }: ISidebarLogo) {
 	return (
 		<a href='/dashboard' className={styles.logo}>
 			{sidebar === SidebarStatus.NORMAL ? (
-				<img
-					draggable={false}
-					src='/logo.png'
-					className='my-4 max-h-10 object-cover'
-				/>
+				<div className='flex items-center justify-center gap-2'>
+					<img
+						draggable={false}
+						src='/icon_desk.png'
+						className='my-4 max-h-10 object-cover'
+					/>
+					<span className='text-2xl font-semibold tracking-widest opacity-65'>
+						Bloomify
+					</span>
+				</div>
 			) : (
 				<>
 					{sidebar === SidebarStatus.MOBILE ? null : (
-						<h5 className='w-full p-5 text-center'>B</h5>
+						<img
+							draggable={false}
+							src='/icon.png'
+							className='mx-auto my-4 max-h-10 object-cover'
+						/>
 					)}
 				</>
 			)}
