bfecb7c925655c804d6b0fb9dfd7c97cfff24c7d
Vadim Kiniabaev <thekinv21@gmail.com>
2025-01-20 21:22:08 +0300

    refactor: remove dynamic import plugin from Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
-import dynamicImport from 'vite-plugin-dynamic-import'
 
 export default defineConfig({
-	plugins: [react(), dynamicImport()],
+	plugins: [react()],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src')
