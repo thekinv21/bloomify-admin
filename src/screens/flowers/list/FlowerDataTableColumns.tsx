3a9712cda43973ac3e8d63761856b69584477d44
Vadim Kiniabaev <thekinv21@gmail.com>
2025-01-22 17:31:22 +0300

    feat: [BMF-31][Vadim]: implement flower management data table with CRUD actions and search functionality

--- src/screens/flowers/list/FlowerDataTableColumns.tsx
+++ src/screens/flowers/list/FlowerDataTableColumns.tsx
@@ -1,10 +1,145 @@
 import { ColumnDef } from '@tanstack/react-table'
+import { FilePenLineIcon, Trash2Icon } from 'lucide-react'
 import { useMemo } from 'react'
+
+import { useCookie, useTranslate } from '@/hooks'
 
 import { IFlower } from '@/types'
 
+import { Alert, Switch } from '@/components/ui'
+
+import { useDeleteFlower } from '../hooks/useDeleteFlower'
+import { useToggleFlower } from '../hooks/useToggleFlower'
+
+import { DateShowFormat } from '@/utils'
+
 export const FlowerDataTableColumns = () => {
-	const columns = useMemo<ColumnDef<IFlower>[]>(() => [], [])
+	const { t } = useTranslate()
+
+	const { isAdmins } = useCookie()
+
+	const { toggleFlower } = useToggleFlower()
+	const { deleteFlower } = useDeleteFlower()
+
+	const columns = useMemo<ColumnDef<IFlower>[]>(
+		() => [
+			{
+				accessorKey: 'imageUrl',
+				header: () => <>{t('image')}</>,
+				cell: info => () => (
+					<img
+						src={info.getValue() as string}
+						alt='flower'
+						className='h-12 w-12 rounded-full object-cover'
+					/>
+				)
+			},
+
+			{
+				accessorKey: 'title',
+				header: () => <>{t('title')}</>,
+				cell: info => info.getValue()
+			},
+
+			{
+				accessorKey: 'description',
+				header: () => <>{t('description')}</>,
+				cell: info => info.getValue()
+			},
+
+			{
+				accessorKey: 'price',
+				header: () => <>{t('price')}</>,
+				cell: info => info.getValue()
+			},
+
+			{
+				accessorKey: 'currency',
+				header: () => <>{t('currency')}</>,
+				cell: info => info.getValue()
+			},
+
+			{
+				accessorKey: 'height',
+				header: () => <>{t('height')}</>,
+				cell: info => info.getValue()
+			},
+
+			{
+				accessorKey: 'width',
+				header: () => <>{t('width')}</>,
+				cell: info => info.getValue()
+			},
+
+			{
+				accessorKey: 'createdAt',
+				header: () => <>{t('createdAt')}</>,
+				cell: info => <>{DateShowFormat(info.getValue() as string)}</>
+			},
+
+			{
+				accessorKey: 'updatedAt',
+				header: () => <>{t('updatedAt')}</>,
+				cell: info => <>{DateShowFormat(info.getValue() as string)}</>
+			},
+
+			{
+				accessorKey: 'isActive',
+				header: () => <>{t('status')}</>,
+				cell: info => {
+					const id = info.row.original?.id
+
+					return (
+						<Switch
+							checked={info.getValue() as boolean}
+							onClick={() => {
+								Alert({
+									subTitle: t('toggle_confirm'),
+									action: async () => {
+										return toggleFlower(id)
+									}
+								})
+							}}
+						/>
+					)
+				}
+			},
+
+			{
+				accessorKey: 'id',
+				header: () => <span>{t('actions')}</span>,
+				cell: info => {
+					const id = info.getValue() as string
+					return (
+						<div className='flex items-start gap-x-5'>
+							{isAdmins && (
+								<button title={t(`edit`)}>
+									<FilePenLineIcon color='blue' size={20} />
+								</button>
+							)}
+
+							{isAdmins && (
+								<button
+									title={t(`delete`)}
+									onClick={() => {
+										Alert({
+											subTitle: t('delete_confirm'),
+											action: async () => {
+												return deleteFlower(+id)
+											}
+										})
+									}}
+								>
+									<Trash2Icon color='red' size={20} />
+								</button>
+							)}
+						</div>
+					)
+				}
+			}
+		],
+		[]
+	)
 
 	return {
 		columns
--- src/screens/flowers/list/index.tsx
+++ src/screens/flowers/list/index.tsx
@@ -1,8 +1,50 @@
+import { PaginationState } from '@tanstack/react-table'
+import { AxiosError } from 'axios'
 import { FlowerIcon } from 'lucide-react'
+import React from 'react'
 
-import { BreadCrumb } from '@/components/ui'
+import { useDebounce, useTranslate } from '@/hooks'
+
+import { AlertCustomEnum, AlertEnum } from '@/types/custom.enum'
+
+import { errorCatch } from '@/services'
+
+import { AlertNotification, BreadCrumb, DataTable } from '@/components/ui'
+
+import { useFetchFlowers } from '../hooks/useFetchFlowers'
+
+import { FlowerDataTableColumns } from './FlowerDataTableColumns'
 
 export function FlowerList() {
+	const { t } = useTranslate()
+
+	const { columns } = FlowerDataTableColumns()
+
+	const [searchTerm, setSearchTerm] = React.useState<string>('')
+
+	const [pagination, setPagination] = React.useState<PaginationState>({
+		pageIndex: 0,
+		pageSize: 10
+	})
+
+	const debouncedSearch = useDebounce(searchTerm, 1500)
+
+	const { query } = useFetchFlowers({
+		page: pagination.pageIndex,
+		size: pagination.pageSize,
+		searchTerm: debouncedSearch.trim() !== '' ? debouncedSearch : undefined
+	})
+
+	React.useEffect(() => {
+		if (query.isError) {
+			return AlertNotification({
+				icon: AlertEnum.WARNING,
+				message: errorCatch(query.error as AxiosError),
+				customClass: AlertCustomEnum.WARNING
+			})
+		}
+	}, [query.isError])
+
 	return (
 		<>
 			<BreadCrumb
@@ -13,6 +55,16 @@ export function FlowerList() {
 						icon: <FlowerIcon size={16} />
 					}
 				]}
+			/>
+
+			<DataTable
+				columns={columns}
+				tableHeading={t('flowers_list')}
+				query={query}
+				pagination={pagination}
+				setPagination={setPagination}
+				searchTerm={searchTerm}
+				setSearchTerm={setSearchTerm}
 			/>
 		</>
 	)
